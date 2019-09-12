fd8b13860961cfa7c0a04d6084f87b1a1db4968c
yeti <jub45@yandex.ru>
2019-09-13 01:46:10 +0300

    off sudo mode

--- update_domain.sh
+++ update_domain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo certbot renew --preferred-challenges=dns --cert-name $1 --force-renew --manual-auth-hook ./authentificator.sh --manual-public-ip-logging-ok
+certbot renew --preferred-challenges=dns --cert-name $1 --force-renew --manual-auth-hook ./authentificator.sh --manual-public-ip-logging-ok
